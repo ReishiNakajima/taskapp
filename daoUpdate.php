f239ca49878122a410024625c961edafd5cb8cd3
Reishi-Nakajima <nakajima.reishi@isid.co.jp>
2018-11-27 10:57:30 +0900

    メンバ変数

--- daoUpdate.php
+++ daoUpdate.php
@@ -11,7 +11,7 @@ class daoUpdate extends dao
 
     public function updateTaskInfo($name, $deadline, $note, $id)
     {
-        $stmt = $db->prepare('UPDATE `task` SET `name` = (?), `deadline` = (?), `note` = (?) WHERE (`id` = (?))');
+        $stmt = $this->db->prepare('UPDATE `task` SET `name` = (?), `deadline` = (?), `note` = (?) WHERE (`id` = (?))');
         $result = $stmt->execute(array($name, $deadline, $note, $id));
         return $result;
     }
