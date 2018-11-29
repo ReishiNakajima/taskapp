1c0b8aa1ecd6d5dda0acd00a9e03948253cf0aac
ReishiNakajima <reishinakajima@gmailcom>
2018-11-29 20:52:18 +0900

    view修正

--- ajaxGetHtml.php
+++ ajaxGetHtml.php
@@ -3,16 +3,20 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
 include 'entity/task.php';
+include 'entity/task.php';
+include 'daoQuery.php';
+include_once 'view/taskCard.php';
 
+$userId = 2;
+$daoQuery = new daoQuery();
 
 switch ($_GET['q']) {
     case 'undoneTaskCardList':
-    include_once 'view/taskCard.php';
-    for ($i = 0; $i < count($undoneTasks); $i++) {
-        print viewTaskCard($undoneTasks[$i]);
-    }
+        $undoneTasks = $daoQuery->queryTaskList($userId, 0, 0);
+        for ($i = 0; $i < count($undoneTasks); $i++) {
+            print viewTaskCard($undoneTasks[$i]);
+        }
         break;
-    
     default:
         echo '定義されていないメソッド呼び出しクエリです';
         break;
